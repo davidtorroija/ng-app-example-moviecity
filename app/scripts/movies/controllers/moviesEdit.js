9c2f950dc04f6314cf0de42318171c95919901d4
David Torroija <david.torroija@provedatos.com>
2014-07-09 07:05:02 -0500

    termine

--- app/scripts/movies/controllers/moviesEdit.js
+++ app/scripts/movies/controllers/moviesEdit.js
@@ -8,15 +8,25 @@
  * Controller of the moviecityApp
  */
 angular.module('moviecityApp')
-  .controller('MoviesEditCtrl', function ($scope, movie,movieModel, actors, genres) {
-    movie.year = moment(movie.releaseYear).format('YYYY');
+  .controller('MoviesEditCtrl', function ($scope, movie,movieModel, actors, genres,$location) {
+
     $scope.movie = movie;
     $scope.actors = actors;
     $scope.genres = genres;
 
 
+    movie.year = moment(movie.releaseYear).format('YYYY');
+    movie.actors = _(movie.actors).pluck('id');
+
     $scope.save = function(){
-      movieModel.save(movie);
+      movie.actors = _(actors).filter(function(actor){
+        return _(movie.actors).contains(actor.id);
+      });
+      movieModel.save(movie)
+      .then(function(){
+        $location.path('/movies/' + movie.id);
+      });
     };
+
 
   });
